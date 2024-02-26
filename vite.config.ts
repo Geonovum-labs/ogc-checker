175bf4a511dbc5b32f45e2a63db6b53244e25cf1
Joost Farla <joost@qonduct.io>
2024-02-26 16:28:35 +0000

    Fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/json-fg-linter/',
   build: {
     outDir: 'docs',
   },
