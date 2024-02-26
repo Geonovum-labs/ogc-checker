85cfe16f424f29354a724f59d79afc5769c86757
Joost Farla <joost@qonduct.io>
2024-02-26 19:28:30 +0000

    Requirement 5 rules

--- src/validation/rules.ts
+++ src/validation/rules.ts
@@ -1,4 +1,7 @@
+import { DateTime, Settings } from 'luxon';
 import { Rule } from './ruleValidation';
+
+Settings.defaultZone = 'utc';
 
 const rules: Rule[] = [];
 
@@ -6,14 +9,76 @@ rules.push({
   name: '/req/core/instant-and-interval',
   validateFeature: feature => {
     if (feature.time !== null) {
-      const { date, timestamp } = feature.time;
+      const { date, timestamp, interval } = feature.time;
 
       if (date !== undefined && timestamp !== undefined && !timestamp.startsWith(date)) {
         return {
           pointer: '/time',
           message:
-            'If the "time" object in any JSON-FG feature in the JSON document includes both a "date" and a "timestamp" member, the full-date parts SHALL be identical.',
+            'If the "time" object in any JSON-FG feature in the JSON document includes both a "date" and a "timestamp" ' +
+            'member, the full-date parts SHALL be identical.',
         };
+      }
+
+      if (timestamp !== undefined && interval !== undefined) {
+        const ts = DateTime.fromISO(timestamp);
+        const tsDate = ts.startOf('day');
+
+        if (
+          (interval[0].length === 10 || interval[1].length === 10) &&
+          ((interval[0] !== '..' && tsDate < DateTime.fromISO(interval[0])) ||
+            (interval[1] !== '..' && tsDate > DateTime.fromISO(interval[1])))
+        ) {
+          return {
+            pointer: '/time',
+            message:
+              'If the "time" object in any JSON-FG feature in the JSON document includes both a "timestamp" and an ' +
+              '"interval" member with start/end dates, the interval SHALL contain the date of the timestamp.',
+          };
+        }
+
+        if (
+          (interval[0].length > 10 || interval[1].length > 10) &&
+          ((interval[0] !== '..' && ts < DateTime.fromISO(interval[0])) ||
+            (interval[1] !== '..' && ts > DateTime.fromISO(interval[1])))
+        ) {
+          return {
+            pointer: '/time',
+            message:
+              'If the "time" object in any JSON-FG feature in the JSON document includes both a "timestamp" and an ' +
+              '"interval" member with start/end timestamps, the interval SHALL contain the timestamp.',
+          };
+        }
+      }
+
+      if (date !== undefined && interval !== undefined) {
+        const tsDate = DateTime.fromISO(date);
+
+        if (
+          (interval[0].length === 10 || interval[1].length === 10) &&
+          ((interval[0] !== '..' && tsDate < DateTime.fromISO(interval[0])) ||
+            (interval[1] !== '..' && tsDate > DateTime.fromISO(interval[1])))
+        ) {
+          return {
+            pointer: '/time',
+            message:
+              'If the "time" object in any JSON-FG feature in the JSON document includes both a "date" and an ' +
+              '"interval" member with start/end dates, the interval SHALL contain the date.',
+          };
+        }
+
+        if (
+          (interval[0].length > 10 || interval[1].length > 10) &&
+          ((interval[0] !== '..' && tsDate < DateTime.fromISO(interval[0]).startOf('day')) ||
+            (interval[1] !== '..' && tsDate > DateTime.fromISO(interval[1]).startOf('day')))
+        ) {
+          return {
+            pointer: '/time',
+            message:
+              'If the "time" object in any JSON-FG feature in the JSON document includes both a "date" and an ' +
+              '"interval" member with start/end timestamps, the interval SHALL include timestamps on the date.',
+          };
+        }
       }
     }
   },
