77dd6ea3d3486b94a956d19286e5b7c454ba5aa5
Joost Farla <joost@qonduct.io>
2024-02-27 15:08:12 +0000

    Increase coverage for 3D metadata

--- src/validation/rules/3d-metadata.test.ts
+++ src/validation/rules/3d-metadata.test.ts
@@ -4,6 +4,18 @@ import metadata from './3d-metadata';
 import { CC_CORE_URI } from './core-metadata';
 
 describe('Requirement 14A', () => {
+  test('Succeeds when a feature does not contain a 3D geometry and does not include the 3D conformance class', () => {
+    const violations = applyRules(metadata, {
+      type: DocumentTypes.FEATURE,
+      conformsTo: [CC_CORE_URI],
+      place: {
+        type: 'Polygon',
+      },
+    } as Feature);
+
+    expect(violations.length).toBe(0);
+  });
+
   test('Fails when a feature contains a 3D geometry and does not include the 3D conformance class', () => {
     const violations = applyRules(metadata, {
       type: DocumentTypes.FEATURE,
@@ -14,6 +26,23 @@ describe('Requirement 14A', () => {
     } as Feature);
 
     expect(violations.length).toBe(1);
+  });
+
+  test('Succeeds when a feature collection does not contain a 3D geometry and does not include the 3D conformance class', () => {
+    const violations = applyRules(metadata, {
+      type: DocumentTypes.FEATURE_COLLECTION,
+      conformsTo: [CC_CORE_URI],
+      features: [
+        {
+          type: DocumentTypes.FEATURE,
+          place: {
+            type: 'Polygon',
+          },
+        },
+      ],
+    } as FeatureCollection);
+
+    expect(violations.length).toBe(0);
   });
 
   test('Fails when a feature collection contains a 3D geometry and does not include the 3D conformance class', () => {
