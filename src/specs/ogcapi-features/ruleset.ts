64416dd204345fad58b8c47d45e27063106d4196
Joost Farla <joost@qonduct.io>
2024-07-10 16:26:13 +0100

    Custom ruleset for ogcapi-features

--- src/specs/ogcapi-features/ruleset.ts
+++ src/specs/ogcapi-features/ruleset.ts
@@ -1,8 +1,12 @@
 import { RulesetDefinition } from '@stoplight/spectral-core';
+import { oas3_0 } from '@stoplight/spectral-formats';
 import { oas } from '@stoplight/spectral-rulesets';
 
 const ruleset: RulesetDefinition = {
-  extends: oas as RulesetDefinition,
+  formats: [oas3_0],
+  rules: {
+    'oas3-schema': oas.rules['oas3-schema'],
+  },
 };
 
 export default ruleset;
