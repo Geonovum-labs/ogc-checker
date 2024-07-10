25258c5bc535bdbc120521b753a4e69bdf86c0f4
Joost Farla <joost@qonduct.io>
2024-07-10 15:37:09 +0100

    Re-usable linter module for Spectral

--- src/specs/ogcapi-features/spec.ts
+++ src/specs/ogcapi-features/spec.ts
@@ -1,46 +1,13 @@
-import { linter } from '@codemirror/lint';
-import { Document, Spectral } from '@stoplight/spectral-core';
-import { Json } from '@stoplight/spectral-parsers';
-import { DiagnosticSeverity } from '@stoplight/types';
-import { Severity, Spec } from '../../types';
+import { spectralLinter } from '../../spectral';
+import { Spec } from '../../types';
 import example from './example.json';
 import ruleset from './ruleset';
-
-const spectral = new Spectral();
-spectral.setRuleset(ruleset);
-
-const mapSeverity = (severity: DiagnosticSeverity): Severity => {
-  switch (severity) {
-    case DiagnosticSeverity.Warning:
-      return 'warning';
-    case DiagnosticSeverity.Information:
-      return 'info';
-    case DiagnosticSeverity.Hint:
-      return 'hint';
-    default:
-      return 'error';
-  }
-};
-
-const spectralLinter = linter(view => {
-  const doc = view.state.doc;
-  const document = new Document(doc.toString(), Json);
-
-  return spectral.run(document).then(violations =>
-    violations.map(violation => ({
-      from: doc.line(violation.range.start.line + 1).from + violation.range.start.character,
-      to: doc.line(violation.range.end.line + 1).from + violation.range.end.character,
-      severity: mapSeverity(violation.severity),
-      message: violation.message,
-    }))
-  );
-});
 
 const spec: Spec = {
   name: 'OGC API Features',
   slug: 'ogcapi-features',
   example: JSON.stringify(example, undefined, 2),
-  linters: [spectralLinter],
+  linters: [spectralLinter(ruleset)],
 };
 
 export default spec;
---
+++ src/spectral.ts
@@ -0,0 +1,37 @@
+import { linter } from '@codemirror/lint';
+import { Document, RulesetDefinition, Spectral } from '@stoplight/spectral-core';
+import { Json } from '@stoplight/spectral-parsers';
+import { DiagnosticSeverity } from '@stoplight/types';
+import { Severity } from './types';
+
+const mapSeverity = (severity: DiagnosticSeverity): Severity => {
+  switch (severity) {
+    case DiagnosticSeverity.Warning:
+      return 'warning';
+    case DiagnosticSeverity.Information:
+      return 'info';
+    case DiagnosticSeverity.Hint:
+      return 'hint';
+    default:
+      return 'error';
+  }
+};
+
+export const spectralLinter = (ruleset: RulesetDefinition) => {
+  const spectral = new Spectral();
+  spectral.setRuleset(ruleset);
+
+  return linter(view => {
+    const doc = view.state.doc;
+    const document = new Document(doc.toString(), Json);
+
+    return spectral.run(document).then(violations =>
+      violations.map(violation => ({
+        from: doc.line(violation.range.start.line + 1).from + violation.range.start.character,
+        to: doc.line(violation.range.end.line + 1).from + violation.range.end.character,
+        severity: mapSeverity(violation.severity),
+        message: violation.message,
+      }))
+    );
+  });
+};
