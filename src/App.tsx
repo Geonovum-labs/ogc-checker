538f3e96a5fa9bb33357ca707e0df4389ba9ef46
Joost Farla <joost@qonduct.io>
2024-07-10 10:02:47 +0100

    Title change

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: FC = () => (
   <div className="flex flex-col h-screen">
     <header className="flex px-4 py-2 bg-slate-700 text-white">
       <div>
-        <h1 className="font-semibold">JSON-FG Linter</h1>
+        <h1 className="font-semibold">OGC Checker</h1>
       </div>
       <div className="ml-auto">
         <a href="https://github.com/Geonovum-labs/json-fg-linter" target="_blank">
