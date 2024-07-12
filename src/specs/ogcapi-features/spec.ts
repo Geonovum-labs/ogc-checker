ae0dfb1419b49ef32cf097bd9599545205c20de7
Joost Farla <joost@qonduct.io>
2024-07-12 11:27:19 +0100

    Support landing page URIs

--- src/specs/ogcapi-features/spec.ts
+++ src/specs/ogcapi-features/spec.ts
@@ -1,18 +1,43 @@
+import { APPLICATION_OPENAPI_JSON_3_0_TYPE } from '../../constants';
 import { spectralLinter } from '../../spectral';
-import { Spec } from '../../types';
+import { Spec, SpecResponseMapper } from '../../types';
 import example from './example.json';
 import ruleset from './ruleset';
+
+const responseMapper: SpecResponseMapper = responseText => {
+  let document;
+
+  try {
+    document = JSON.parse(responseText);
+  } catch (err) {
+    return Promise.resolve({ content: responseText });
+  }
+
+  const links = document.links;
+
+  if (Array.isArray(links)) {
+    const serviceDescLink = links.find(
+      link => link.rel === 'service-desc' && link.type === APPLICATION_OPENAPI_JSON_3_0_TYPE
+    );
+
+    if (serviceDescLink) {
+      return fetch(serviceDescLink.href, {
+        headers: { Accept: serviceDescLink.type },
+      })
+        .then(res => res.text())
+        .then(content => ({ content }));
+    }
+  }
+
+  return Promise.resolve({ content: responseText });
+};
 
 const spec: Spec = {
   name: 'OGC API - Features',
   slug: 'ogcapi-features',
   example: JSON.stringify(example, undefined, 2),
   linters: [spectralLinter(ruleset)],
-  responseMapper: responseText =>
-    Promise.resolve({
-      content: responseText,
-      additionaLinters: [],
-    }),
+  responseMapper,
 };
 
 export default spec;
