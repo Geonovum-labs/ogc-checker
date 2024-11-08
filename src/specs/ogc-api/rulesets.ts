cc130bebacf28d59e75bb8ee93a3e3df91d5a1c1
Joost Farla <joost@qonduct.io>
2024-11-08 13:03:06 +0100

    Remove API Common as separate rulesets

--- src/specs/ogc-api/rulesets.ts
+++ src/specs/ogc-api/rulesets.ts
@@ -8,7 +8,6 @@ import responseMatchSchema from '../../functions/responseMatchSchema';
 import { OpenAPIV3_0 } from '../../openapi-types';
 import { errorMessage } from '../../util';
 
-export const API_COMMON_1_OAS3 = 'http://www.opengis.net/spec/ogcapi-common-1/1.0/conf/oas30';
 export const API_FEATURES_1_CORE = 'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core';
 export const API_FEATURES_1_OAS3 = 'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/oas30';
 export const API_FEATURES_1_GEOJSON = 'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/geojson';
@@ -16,28 +15,6 @@ export const API_FEATURES_1_GEOJSON = 'http://www.opengis.net/spec/ogcapi-featur
 interface Rulesets {
   [confClass: string]: RulesetDefinition;
 }
-
-const apiCommon1OpenApi30: RulesetDefinition = {
-  documentationUrl: 'http://www.opengis.net/spec/ogcapi-common-1/1.0/req/oas30',
-  description: 'OGC API - Common - Part 1: Core - Requirements Class "OpenAPI 3.0"',
-  formats: [oas3_0],
-  rules: {
-    '/req/oas30/oas-definition-2': {
-      given: '$',
-      message: 'The JSON representation SHALL conform to the OpenAPI Specification, version 3.0. {{error}}.',
-      severity: 'error',
-      then: [
-        {
-          function: oasDocumentSchema,
-        },
-        {
-          field: 'paths',
-          function: oasPathParam,
-        },
-      ],
-    },
-  },
-};
 
 const apiFeatures1Core: RulesetDefinition = {
   documentationUrl: 'http://www.opengis.net/spec/ogcapi-features-1/1.0/conf/core',
@@ -337,7 +314,6 @@ const apiFeatures1GeoJson: RulesetDefinition = {
 };
 
 const rulesets: Rulesets = {
-  [API_COMMON_1_OAS3]: apiCommon1OpenApi30,
   [API_FEATURES_1_CORE]: apiFeatures1Core,
   [API_FEATURES_1_OAS3]: apiFeatures1OpenApi30,
   [API_FEATURES_1_GEOJSON]: apiFeatures1GeoJson,
