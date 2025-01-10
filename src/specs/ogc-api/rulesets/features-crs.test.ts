6675e929f4b98d7bf3a943bb4f4a94a041d15380
Joost Farla <joost@qonduct.io>
2025-01-10 12:11:47 +0100

    Clone example doc in tests

--- src/specs/ogc-api/rulesets/features-crs.test.ts
+++ src/specs/ogc-api/rulesets/features-crs.test.ts
@@ -1,6 +1,7 @@
 import { Spectral } from '@stoplight/spectral-core';
+import { clone } from 'ramda';
 import { describe, expect, test } from 'vitest';
-import oasDocument from '../example.json';
+import exampleDoc from '../example.json';
 import ruleset from './features-crs';
 
 const spectral = new Spectral();
@@ -8,14 +9,16 @@ spectral.setRuleset(ruleset);
 
 describe('/req/crs/fc-md-crs-list', () => {
   test('Succeeds when the collection object schema sets the "crs" property as required', async () => {
-    const violations = await spectral.run(oasDocument);
+    const oasDoc = clone(exampleDoc);
+    const violations = await spectral.run(oasDoc);
 
     expect(violations).toHaveLength(0);
   });
 
   test('Fails when the collection object schema does not set the "crs" property as required', async () => {
-    oasDocument.components.schemas.collection.required = ['id', 'links'];
-    const violations = await spectral.run(oasDocument);
+    const oasDoc = clone(exampleDoc);
+    oasDoc.components.schemas.collection.required = ['id', 'links'];
+    const violations = await spectral.run(oasDoc);
 
     expect(violations).toContainViolation('/req/crs/fc-md-crs-list', 2);
   });
