3579dd5ffcf934de7ec7ad8966c5e604ce9cc045
Joost Farla <joost@qonduct.io>
2025-05-22 13:56:25 +0200

    Fix conformsTo discovery

--- src/specs/ogc-api/spec.ts
+++ src/specs/ogc-api/spec.ts
@@ -6,54 +6,56 @@ import featuresExample from './examples/features.json';
 import recordsExample from './examples/records.json';
 import rulesets from './rulesets';
 
-const responseMapper: SpecResponseMapper = async responseText => {
-  let document;
+const responseMapper =
+  (prefix: string): SpecResponseMapper =>
+  async responseText => {
+    let document;
 
-  try {
-    document = JSON.parse(responseText);
-  } catch {
-    return Promise.resolve({ content: responseText });
-  }
-
-  const links = document.links;
-
-  if (Array.isArray(links)) {
-    const serviceDescLink = links.find(link => link.rel === 'service-desc' && link.type === APPLICATION_OPENAPI_JSON_3_0_TYPE);
-
-    const conformanceLink = links.find(link => link.rel === 'conformance');
-
-    if (serviceDescLink) {
-      const content = await fetch(serviceDescLink.href, {
-        headers: { Accept: serviceDescLink.type },
-      }).then(response => handleResponse(response, serviceDescLink.href));
-
-      const linters: SpecLinter[] = [];
-
-      if (conformanceLink) {
-        const conformance = await fetch(conformanceLink.href, {
-          headers: { Accept: APPLICATION_JSON_TYPE },
-        }).then(response => handleResponseJson(response, conformanceLink.href));
-
-        const conformsTo = conformance.conformsTo;
-
-        if (Array.isArray(conformsTo)) {
-          conformsTo.forEach(reqClass => {
-            if (typeof reqClass === 'string' && rulesets[reqClass]) {
-              linters.push({
-                name: reqClass,
-                linter: spectralLinter(reqClass, rulesets[reqClass]),
-              });
-            }
-          });
-        }
-      }
-
-      return { content, linters };
+    try {
+      document = JSON.parse(responseText);
+    } catch {
+      return Promise.resolve({ content: responseText });
     }
-  }
 
-  return Promise.resolve({ content: responseText });
-};
+    const links = document.links;
+
+    if (Array.isArray(links)) {
+      const serviceDescLink = links.find(link => link.rel === 'service-desc' && link.type === APPLICATION_OPENAPI_JSON_3_0_TYPE);
+
+      const conformanceLink = links.find(link => link.rel === 'conformance');
+
+      if (serviceDescLink) {
+        const content = await fetch(serviceDescLink.href, {
+          headers: { Accept: serviceDescLink.type },
+        }).then(response => handleResponse(response, serviceDescLink.href));
+
+        const linters: SpecLinter[] = [];
+
+        if (conformanceLink) {
+          const conformance = await fetch(conformanceLink.href, {
+            headers: { Accept: APPLICATION_JSON_TYPE },
+          }).then(response => handleResponseJson(response, conformanceLink.href));
+
+          const conformsTo = conformance.conformsTo;
+
+          if (Array.isArray(conformsTo)) {
+            conformsTo.forEach(reqClass => {
+              if (typeof reqClass === 'string' && rulesets[reqClass] && reqClass.startsWith(prefix)) {
+                linters.push({
+                  name: reqClass,
+                  linter: spectralLinter(reqClass, rulesets[reqClass]),
+                });
+              }
+            });
+          }
+        }
+
+        return { content, linters };
+      }
+    }
+
+    return Promise.resolve({ content: responseText });
+  };
 
 const linterName = (confClass: string) => confClass.replace('http://www.opengis.net/spec/', '');
 
@@ -67,7 +69,7 @@ export const ogcApiFeaturesSpec: Spec = {
       name: linterName(entry[0]),
       linter: spectralLinter(linterName(entry[0]), entry[1]),
     })),
-  responseMapper,
+  responseMapper: responseMapper('http://www.opengis.net/spec/ogcapi-features-'),
 };
 
 export const ogcApiRecordsSpec: Spec = {
@@ -80,5 +82,5 @@ export const ogcApiRecordsSpec: Spec = {
       name: linterName(entry[0]),
       linter: spectralLinter(linterName(entry[0]), entry[1]),
     })),
-  responseMapper,
+  responseMapper: responseMapper('http://www.opengis.net/spec/ogcapi-records-'),
 };
