ec828321d37ae9cd67a4343f17b28b92d9443335
Joost Farla <joost@qonduct.io>
2025-10-09 14:11:43 +0200

    JSON completion + add /req/ogc-process-description/json-encoding

---
+++ src/specs/ogc-api/rulesets/processes-ogc-process-description.ts
@@ -0,0 +1,30 @@
+import type { RulesetDefinition } from '@stoplight/spectral-core';
+import { oas3_0 } from '@stoplight/spectral-formats';
+import hasSchemaMatch from '../../../functions/hasSchemaMatch';
+
+export const OGC_API_PROCESSES_OGC_PROCESS_DESCRIPTION_URI =
+  'http://www.opengis.net/spec/ogcapi-processes-1/1.0/conf/ogc-process-description';
+
+export const OGC_API_PROCESSES_OGC_PROCESS_DESCRIPTION_DOC_URI = 'https://docs.ogc.org/DRAFTS/18-062r3.html#req_ogc-process-description_';
+
+const processOgcProcessDescription: RulesetDefinition = {
+  documentationUrl: 'http://www.opengis.net/spec/ogcapi-processes-1/1.0/req/ogc-process-description',
+  description: 'OGC API - Processes - Part 1: Core - Requirements Class "OGC Process Description"',
+  formats: [oas3_0],
+  rules: {
+    '/req/ogc-process-description/json-encoding': {
+      given: '$.paths[?(@property.match(/^\\/processes\\/[^/]+$/))].get.responses.200',
+      message: '200-responses of the server SHALL support the "application/json" media type. {{error}}',
+      documentationUrl: OGC_API_PROCESSES_OGC_PROCESS_DESCRIPTION_DOC_URI + 'json-encoding',
+      severity: 'error',
+      then: {
+        function: hasSchemaMatch,
+        functionOptions: {
+          schemaUri: 'https://schemas.opengis.net/ogcapi/processes/part1/1.0/openapi/schemas/process.yaml',
+        },
+      },
+    },
+  },
+};
+
+export default processOgcProcessDescription;
